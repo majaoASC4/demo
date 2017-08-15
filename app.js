d05cbb441fbd6f857a7400506099e316cf138713
majaoASC4 <robloxphillz@gmail.com>
2017-08-14 23:35:05 -0400

    Hmmm Maybe Angular?

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const displayProfile = (user) => {
 
 const displayJobListings = () => {
      let info = checkInput()
+     let jobListData = firebase.database().ref("jobs")
      $.ajax({
           dataType: "jsonp",
           url: "https://jobs.github.com/positions.json?"+info,
@@ -55,6 +56,13 @@ const displayJobListings = () => {
                          </div>
                     `)
                }
+          }
+     })
+     $.ajax({
+          dataType: "jsonp",
+          url: "https://service.dice.com/api/rest/jobsearch/v1/simple.json",
+          success: function(data){
+               console.log(data)
           }
      })
 }
@@ -114,6 +122,7 @@ $(() => {
      $("#search").keypress(function(e){
           if(e.which == 13){
                $(".profile-render").addClass("disappear")
+               $(".admin-panel").addClass("disappear")
                displayJobListings()
                $(".job-listings").removeClass("disappear")
           }
@@ -121,4 +130,56 @@ $(() => {
      $(".form-inline").submit(function(e){
           e.preventDefault();
      })
+     let jobListData = firebase.database().ref("jobs")
+     jobListData.on('child_added', function(row){
+          let job = row.val()
+          $(".custom-list").append(`
+               <div class="col-md-4 col-sm-12">
+                    <div class="job-card card">
+                         <img class="card-img-top" src="`+job.comp_logo+`">
+                         <div class="card-body">
+                              <h4 class="card-title">`+job.comp_name+`</h4>
+                              <p class="card-text">`+job.job_desc.substring(0,150)+"..."+`</p>
+                              <a href="`+job.comp_site+`">Company Website</a>
+                              <a>`+job.location+`</a>
+                         </div>
+                    </div>
+               </div>
+          `)
+     })
+})
+
+$("#job-add").click(function(e){
+     e.preventDefault();
+     $(".profile-render").addClass("disappear")
+     $(".job-listings").addClass("disappear")
+     $(".admin-panel").removeClass("disappear")
+})
+
+$("#home").click(function(e){
+     e.preventDefault();
+     $(".admin-panel").addClass("disappear")
+     $(".job-listings").addClass("disappear")
+     $(".profile-render").removeClass("disappear")
+})
+
+$("#admin-panel-submit").click(function(){
+     let jobName = $("#job-name").val()
+     let compName = $("#company-name").val()
+     let compLogo = $("#company-logo").val()
+     let compSite = $("#company-site").val()
+     let jobDesc = $("#job-description").val()
+     let recruiter = $("#offerer").val()
+     if((jobName && compName && compLogo && compSite && jobDesc && recruiter) && (jobName != "" && compName != "" && compLogo != "" && jobDesc != "" && recruiter != "")){
+          firebase.database().ref("jobs").push({
+               job_name: jobName,
+               comp_name: compName,
+               job_desc: jobDesc,
+               comp_logo: compLogo,
+               offerer: recruiter,
+               comp_site: compSite,
+               location: "New York"
+          })
+          $("input").val("")
+     }
 })
